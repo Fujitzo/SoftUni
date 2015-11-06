3b41340ba53c826fd770855136f78435837f1520
Fujitzo <xpucto87@gmail.com>
2015-11-06 23:52:55 +0200

    Board with dots and collision detection (walls)

---
+++ Java/Pacman Game/src/Pacman.java
@@ -0,0 +1,141 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+
+public class Pacman extends Canvas implements Runnable, KeyListener {
+
+    private boolean isRunning = false;
+
+    public static final int width = 640;
+    public static final int height = 480;
+    public static final String title = "PacMan";
+
+    private Thread thread;
+
+    public static Player player;
+    public static Level level;
+
+    public Pacman(){
+        Dimension dimension = new Dimension(Pacman.width, Pacman.height);
+        setPreferredSize(dimension);
+        setMinimumSize(dimension);
+        setMaximumSize(dimension);
+
+        addKeyListener(this);
+        player = new Player(Pacman.width/2 , Pacman.height/2);
+        level = new Level("map.png");
+
+    }
+
+    public synchronized void start(){
+        if(isRunning){
+            return;
+        }
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public synchronized void stop(){
+        if(!isRunning){
+            return;
+        }
+        isRunning = false;
+        try {
+            thread.join();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void tick(){
+        //System.out.println("Working");
+        player.tick();
+        level.tick();
+    }
+
+    private void render(){
+        BufferStrategy bs = getBufferStrategy();
+        if(bs ==null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics graphics = bs.getDrawGraphics();
+        graphics.setColor(Color.black);
+        graphics.fillRect(0,0, Pacman.width, Pacman.height);
+        player.render(graphics);
+        level.render(graphics);
+        graphics.dispose();
+        bs.show();
+
+    }
+    @Override
+    public void run(){
+        requestFocus();
+
+        int fps = 0;
+        double timer = System.currentTimeMillis();
+
+        long lastTime = System.nanoTime();
+        double delta = 0;
+        double targetTick = 60.0;
+        double ns = 1000000000/targetTick;
+
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ ns ;
+            lastTime = now;
+
+            while(delta >= 1){
+                tick();
+                render();
+                fps++;
+                delta--;
+            }
+            if(System.currentTimeMillis() - timer >= 1000){
+                System.out.println(fps);
+                fps = 0;
+                timer += 1000;
+            }
+        }
+        stop();
+    }
+
+    public static void main(String[] args) {
+        Pacman pacman = new Pacman();
+        JFrame frame = new JFrame();
+        frame.setTitle(pacman.title);
+        frame.add(pacman);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        pacman.start();
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT) player.right = true;
+        if(e.getKeyCode()==KeyEvent.VK_LEFT) player.left = true;
+        if(e.getKeyCode()==KeyEvent.VK_UP) player.up = true;
+        if(e.getKeyCode()==KeyEvent.VK_DOWN) player.down = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT) player.right = false;
+        if(e.getKeyCode()==KeyEvent.VK_LEFT) player.left = false;
+        if(e.getKeyCode()==KeyEvent.VK_UP) player.up = false;
+        if(e.getKeyCode()==KeyEvent.VK_DOWN) player.down = false;
+    }
+}
